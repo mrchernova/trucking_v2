abcf2b6064d557f0671dd5b293dddef58fb99a52
cherraniram@gmail.com <cherraniram@gmail.com>
2022-12-19 21:35:43 +0300

    Initial commit

---
+++ src/main/java/by/project/trucking_v2/TruckingV2Application.java
@@ -0,0 +1,13 @@
+package by.project.trucking_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckingV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckingV2Application.class, args);
+	}
+
+}
