916a3907e915535b0bf8741397d4a7587d5e2a79
cherraniram@gmail.com <cherraniram@gmail.com>
2023-01-07 10:33:32 +0300

    Commit: user + legalEntity controller +logger

--- src/main/java/by/project/trucking_v2/exception/NotFoundException.java
+++ src/main/java/by/project/trucking_v2/exception/NotFoundException.java
@@ -2,12 +2,15 @@ package by.project.trucking_v2.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+import java.util.logging.Logger;
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException{
     public NotFoundException() {
-        super("Мы не знаем что это такое, если бы мы знали что это такое, мы не знаем что это такое");
+        super();
 
-
+        final Logger logger = Logger.getLogger(NotFoundException.class.getName());
+//        logger.fine("fine!");
+        logger.info("Мы не знаем что это такое, если бы мы знали что это такое, мы не знаем что это такое");
+        logger.severe("404. Such id not found");
     }
 }
