8fb5a8d941d99618e60f3f7e92064c2b857f4337
cherraniram@gmail.com <cherraniram@gmail.com>
2023-01-20 23:30:10 +0300

    --------

--- src/main/java/by/project/trucking_v2/controller/HelloController.java
+++ src/main/java/by/project/trucking_v2/controller/HelloController.java
@@ -2,30 +2,36 @@ package by.project.trucking_v2.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello";
+    public String mainPage() {
+        return "index";
     }
 
-    @GetMapping("/user")
-    public String user(Authentication authentication) {
-        System.out.println("это я вывожу из HelloController");
-        System.out.println((UserDetails)authentication.getPrincipal());
-        return "User";
-    }
 
-    @GetMapping("/admin")
+//    @GetMapping("/")
+//    public String user(Authentication authentication) {
+//        try {
+//            System.out.println("это я вывожу из HelloController");
+//            System.out.println("Текущий пользователь: " + (UserDetails) authentication.getPrincipal());
+//            return "Текущий пользователь: " + (UserDetails) authentication.getPrincipal();
+//        }catch(NullPointerException e){
+//            return "index";
+//        }
+//    }
 
-    public String admin(Authentication authentication) {
-        System.out.println("это я вывожу из HelloController");
-        System.out.println((UserDetails)authentication.getPrincipal());
-        return "Admin";
-    }
+//    @GetMapping("/admin")
+//
+//    public String admin(Authentication authentication) {
+//        System.out.println("это я вывожу из HelloController");
+//        System.out.println((UserDetails)authentication.getPrincipal());
+//        return "Admin";
+//    }
 
 }
--- src/main/java/by/project/trucking_v2/security/SecurityConfig.java
+++ src/main/java/by/project/trucking_v2/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import by.project.trucking_v2.model.Role;
 import by.project.trucking_v2.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,12 +37,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     /** метод отвечает за авторизацию */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-//                .antMatchers(HttpMethod.POST, "/api/user").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMINISTRATOR")
-                .antMatchers("/orders").hasRole("CARRIER")
-                .antMatchers("/orders/**").hasAnyRole("CLIENT", "ADMINISTRATOR")
-                .antMatchers("/**").permitAll()
+        http
+                .authorizeRequests()
+//                .cors().disable()
+//                .csrf().disable()
+
+                .antMatchers("/").permitAll()
+
+                .antMatchers(HttpMethod.GET, "/users").hasRole("ADMINISTRATOR")
+                .antMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/users/**").permitAll()
+                .antMatchers(HttpMethod.DELETE,"/users/**").hasAnyRole("ADMINISTRATOR", "CLIENT", "CARRIER")
+
+                .antMatchers(HttpMethod.GET,"/orders").permitAll()
+                .antMatchers(HttpMethod.POST,"/orders/**").hasRole("CLIENT")
+                .antMatchers(HttpMethod.PUT,"/orders/**").hasAnyRole("CLIENT", "ADMINISTRATOR")
+                .antMatchers(HttpMethod.DELETE, "/orders/**").hasAnyRole("CLIENT", "ADMINISTRATOR")
+
+                .antMatchers(HttpMethod.GET,"/drivers").permitAll()
+                .antMatchers(HttpMethod.POST,"/drivers/**").hasRole("CARRIER")
+                .antMatchers(HttpMethod.PUT,"/drivers/**").hasAnyRole("CARRIER", "ADMINISTRATOR")
+                .antMatchers(HttpMethod.DELETE, "/drivers/**").hasAnyRole("CARRIER", "ADMINISTRATOR")
+
+                .antMatchers(HttpMethod.GET,"/transport").permitAll()
+                .antMatchers(HttpMethod.POST,"/transport/**").hasRole("CARRIER")
+                .antMatchers(HttpMethod.PUT,"/transport/**").hasAnyRole("CARRIER", "ADMINISTRATOR")
+                .antMatchers(HttpMethod.DELETE, "/transport/**").hasAnyRole("CARRIER", "ADMINISTRATOR")
+
+                .antMatchers(HttpMethod.GET,"/completed_orders").permitAll()
+                .antMatchers(HttpMethod.POST,"/completed_orders/**").hasAnyRole("ADMINISTRATOR", "CARRIER", "CLIENT")
+                .antMatchers(HttpMethod.PUT,"/completed_orders/**").hasRole("ADMINISTRATOR")
+                .antMatchers(HttpMethod.DELETE, "/completed_orders/**").hasRole("ADMINISTRATOR")
+
+
                 .and().formLogin();
 
     }
